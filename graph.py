b79b868b77c16fe240a88423bedc9736bd336932
jackokeefe11 <37940238+jackokeefe11@users.noreply.github.com>
2023-10-09 02:23:52 -0500

    changing graph.py

--- graph.py
+++ graph.py
@@ -4,33 +4,34 @@ import ID3  # Assuming your ID3 code is in a file named ID3.py
 import parse  # Assuming you have a parse function to read the data
 
 def load_data(file_path):
-    # Assuming you have a function to load and parse your data
     return parse.parse(file_path)
 
-def split_data(data, train_size):
+def split_data(data, train_size, val_size):
     random.shuffle(data)
     train = data[:train_size]
-    test = data[train_size:]
-    return train, test
+    val = data[train_size:train_size+val_size]
+    test = data[train_size+val_size:]
+    return train, val, test
 
 def main():
     data = load_data('house_votes_84.data')
-    training_sizes = range(10, 301, 10)  # Adjust the step as needed
+    training_sizes = range(10, 301, 10)  
     accuracies_with_pruning = []
     accuracies_without_pruning = []
 
+    val_size = 100  
     for size in training_sizes:
         acc_with_pruning = 0
         acc_without_pruning = 0
         for _ in range(100):
-            train, test = split_data(data, size)
+            train, val, test = split_data(data, size, val_size)
             
             # Without Pruning
             tree = ID3.ID3(train, 'democrat')  
             acc_without_pruning += ID3.test(tree, test)
             
             # With Pruning
-            ID3.prune(tree, train)  # Assuming your prune function uses the training data for pruning
+            ID3.prune(tree, val)  # Now pruning with validation data
             acc_with_pruning += ID3.test(tree, test)
         
         accuracies_with_pruning.append(acc_with_pruning / 100)
